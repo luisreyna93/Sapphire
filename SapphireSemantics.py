3d7d111465f7f6774600f9ffec568b1718d3c0be
Azael Alanis <azaelalanis@MacBook-Pro-de-Azael.local>
2015-10-05 21:55:01 -0500

    Cambio de nombre el las funciones y otros pequenios ajustes

--- SapphireSemantics.py
+++ SapphireSemantics.py
@@ -13,15 +13,8 @@ global_vars_dic = {}
 memoryCont = 1000
 
 errors = {
-        'PARAMETER_LENGTH_MISMATCH': 'Function {0} expects {1} parameters and received {2} parameters at line: {3} ',
-        'REPEATED_DECLARATION_FUNC': 'Declaracion de funcion repetida.',
-        'REPEATED_FUNC_DECLARATION': 'Repeated declaration of function {0} found at line: {1} ',
-        'UNDECLARED_VARIABLE': 'Undeclared variable {0} found at line: {1} ',
-        'UNDECLARED_FUNCTION': 'Undeclared function {0} found at line: {1} ',
-        'STACKOVERFLOW': 'Stackoverflow, the program is too big.',
-        'PARAMETER_TYPE_MISMATCH': 'Function {0}, expected type {1} and received type {2} in position {3}',
-        'PARAMETER_LENGTH_MISMATCH': 'Function {0}, expected {1} parameters',
-        'INVALID_ARRAY_DECLARATION': 'Variable {0} of type array in line {1}, should be declared with constant dimensions.'
+        'REPEATED_DECLARATION_VAR': 'REPEATED_DECLARATION_VAR: Declaracion de variable repetida.',
+        'REPEATED_DECLARATION_FUNC': 'REPEATED_DECLARATION_FUNC: Declaracion de funcion repetida.'
 }
 
 def add_to_func(fid, ftype, fparams):
@@ -32,33 +25,6 @@ def add_to_func(fid, ftype, fparams):
         'localVars' : deepcopy(local_var_dict)
     }
     local_var_dict={}
-
-def func_is_repeated(fid):
-    if fid in func_dic:
-        return True
-    else:
-        return False
-def global_var_exists(vid):
-    if vid in global_vars_dic:
-        return True
-    else:
-        return False
-def local_var_exists(vid):
-    if vid in global_vars_dic or vid in local_var_dict:
-        return True
-    else:
-        return False
-def print_func_dict():
-    print "\nFunciones"
-    pp.pprint(func_dic)
-
-def print_local_var_dict():
-    print "\nLOCAL VAR DICT"
-    pp.pprint(local_var_dict)
-
-def print_global_var_dict():
-    print "\nGLOBAL VAR DICT"
-    pp.pprint(global_vars_dic)
 
 def add_to_local_var_dict(var_id, type):
     global memoryCont
@@ -77,3 +43,33 @@ def add_to_global_var_dict(var_id, type):
     }
     print_global_var_dict()
     memoryCont= memoryCont +1
+
+def func_is_repeated(fid):
+    if fid in func_dic:
+        return True
+    else:
+        return False
+
+def global_is_repeated(vid):
+    if vid in global_vars_dic:
+        return True
+    else:
+        return False
+
+def local_is_repeated(vid):
+    if vid in global_vars_dic or vid in local_var_dict:
+        return True
+    else:
+        return False
+
+def print_func_dict():
+    print "\nFunciones"
+    pp.pprint(func_dic)
+
+def print_local_var_dict():
+    print "\nVariables locales"
+    pp.pprint(local_var_dict)
+
+def print_global_var_dict():
+    print "\nVariables globales"
+    pp.pprint(global_vars_dic)
